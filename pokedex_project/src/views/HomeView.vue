ad4503596fb2cae54cea8831e3af909c2ad4485f
rodrigues-leo97 <990562@adm.bancoamazonia.sa>
2024-04-20 20:56:20 -0300

    feat: impl filtro

--- pokedex_project/src/views/HomeView.vue
+++ pokedex_project/src/views/HomeView.vue
@@ -1,45 +1,13 @@
-<script setup>
-import { onMounted, ref } from "vue";
-
-const pokemons = ref([]);
-let offset = 0;
-const limit = 20;
-
-onMounted(async () => {
-  await loadPokemons();
-  window.addEventListener('scroll', handleScroll);
-});
-
-const loadPokemons = async () => {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
-  const data = await response.json();
-  pokemons.value.push(...data.results);
-  offset += limit;
-};
-
-const handleScroll = () => {
-  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-    loadPokemons();
-  }
-};
-</script>
-
 <template>
   <main>
-    <div class="container">
-      <div class="row mt-4">
-        <div class="col-sm-12 col-md-6">
-          <div class="card" style="width: 18rem;">
-            <img src="https://e7.pngegg.com/pngimages/72/948/png-clipart-bulbasaur-pokemon-diamond-and-pearl-pokemon-go-pokedex-ivysaur-bulbasaur-pixel-vertebrate-grass-thumbnail.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-12 col-md-6">
-          <ul>
-            <li v-for="pokemon in pokemons" :key="pokemon.name">
+    <div class="container mt-4">
+      <div>
+        <input type="text" v-model="searchTerm" @input="search" placeholder="Digite para buscar">
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <ul class="list-group">
+            <li class="list-group-item" v-for="pokemon in copyListPokemons" :key="pokemon.name">
               {{ pokemon.name }}
             </li>
           </ul>
@@ -48,3 +16,53 @@ const handleScroll = () => {
     </div>
   </main>
 </template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+
+let data = ref([])
+
+let pokemons = ref([]);
+let copyListPokemons = ref([]);
+let offset = 0;
+const limit = 20;
+let loading = false;
+
+const loadPokemons = async () => {
+  loading = true;
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
+  data = await response.json();
+  pokemons.value.push(...data.results);
+  copyListPokemons.value.push(...data.results);
+  offset += limit;
+  loading = false;
+};
+
+const search = () => {
+  copyListPokemons.value = []
+  if (searchTerm.value !== "") {
+    const filteredData = pokemons.value.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm.value.toLowerCase()));
+    copyListPokemons.value.push(...filteredData);
+    console.log(pokemons)
+  } else {
+    offset = 0
+    pokemons.value = []
+    loadPokemons();
+  }
+};
+const searchTerm = ref('');
+
+const handleScroll = () => {
+  if (loading) return;
+  const bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
+  if (bottomOfWindow) {
+    loadPokemons();
+  }
+};
+
+onMounted(() => {
+  loadPokemons();
+  window.addEventListener('scroll', handleScroll);
+});
+
+</script>
