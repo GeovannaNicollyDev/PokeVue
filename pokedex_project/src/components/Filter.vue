f3c69c528c53f8246c9cf70bf8c47e80843e365c
rodrigues-leo97 <990562@adm.bancoamazonia.sa>
2024-04-21 15:39:46 -0300

    feat: new features

--- pokedex_project/src/components/Filter.vue
+++ pokedex_project/src/components/Filter.vue
@@ -1,29 +1,24 @@
 <template>
-  <main>
-    <div class="container mt-4">
-      <div>
-        <input type="text" 
-          v-model="searchTerm" 
-          @input="search"
-          placeholder="Digite para buscar">
-      </div>
-      <div class="row">
-        <div class="col-md-6">
-          <ul class="list-group">
-            <li class="list-group-item" v-for="pokemon in copyListPokemons" :key="pokemon.name" @click="openModal(pokemon)">
-              {{ pokemon.name }}
-            </li>
-              <Modal :isOpen="isModalOpen.value"/>
-          </ul>
+    <main>
+      <div class="container mt-4">
+        <div>
+          <input type="text" v-model="searchTerm" @input="search" placeholder="Digite para buscar">
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <ul class="list-group">
+              <li class="list-group-item" v-for="pokemon in copyListPokemons" :key="pokemon.name">
+                {{ pokemon.name }}
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  </main>
-</template>
+    </main>
+  </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
-import Modal from "@/components/Modal.vue"
 
 let data = ref([])
 
@@ -32,10 +27,9 @@ let copyListPokemons = ref([]);
 let offset = 0;
 const limit = 20;
 let loading = false;
-const selectedPokemon = ref(null);
-const isModalOpen = ref(false);
 
 const loadPokemons = async () => {
+  loading = true;
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
   data = await response.json();
   pokemons.value.push(...data.results);
@@ -44,18 +38,8 @@ const loadPokemons = async () => {
   loading = false;
 };
 
-const openModal = (pokemon) => {
-  selectedPokemon.value = pokemon;
-  isModalOpen.value = true;
-};
-
-const closeModal = () => {
-  selectedPokemon.value = null;
-  isModalOpen.value = false;
-};
-
 const search = () => {
-  copyListPokemons.value = [];
+  copyListPokemons.value = []
   if (searchTerm.value !== "") {
     const filteredData = pokemons.value.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm.value.toLowerCase()));
     copyListPokemons.value.push(...filteredData);
---
+++ pokedex_project/src/components/Modal.vue
@@ -0,0 +1,27 @@
+<template>
+    <div v-if="isOpen" class="modal">
+        <div class="modal-content">
+            <span class="close" @click="close">&times;</span>
+            <div>
+            <h2>{{ selectedItem.name }}</h2>
+            <p>Outros dados do Pokémon...</p>
+            </div>
+        </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['isOpen', 'selectedItem'],
+    methods: {
+      close() {
+        this.$emit('close');
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  /* Estilos do modal */
+  </style>
+  
